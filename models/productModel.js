643de9d40a1e4693362d504c143b0bfc048a0bf5
Kunal Sapate <113632918+kunal20000@users.noreply.github.com>
2023-12-03 18:02:54 +0530

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -4,12 +4,31 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   // 'name' field is of type String
   // It is required (cannot be empty)
+  name:{
+    type: String,
+    required: true,
+  },
   // 'price' field is of type Number
   // It is required (cannot be empty)
+  price:{
+    type: Number,
+    required: true,
+  },
+
+  
   // 'category' field is of type String
   // It is required (cannot be empty)
+  category:{
+    type: String,
+    required: true,
+  },
+  
   // 'availability' field is of type Boolean
   // It has a default value of 'true'
+  availability:{
+    type: Boolean,
+    default: true,
+  },
 });
 
 // Create a 'Product' model using the defined schema
